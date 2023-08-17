6c6ca672802dc2d7b43f1847b27b3559c214664d
giang <vuthithaogiang050400@gmail.com>
2023-08-17 14:57:53 +0700

    add

--- FirstDemo/slot11Asignment/BankAccount.cs
+++ FirstDemo/slot11Asignment/BankAccount.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace FirstDemo.slot11Asignment
 {
-    internal class BankAccount
+    public delegate void BalanceChangedHandler(decimal newBalance);
+    public class BankAccount
     {
+        private decimal balance;
+
+        public event BalanceChangedHandler BalanceChanged;
+        
+        public BankAccount()
+        {
+            BalanceChanged += SendSMSUpdateBallance;
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                BalanceChanged(amount);
+                SendSMSUpdateBallance(amount);
+                return true;
+            }
+            return false;
+        }
+
+        public bool WithDraw(decimal amount)
+        {
+                if (amount > 0 && amount <= Balance)
+                {
+                    Balance -= amount;
+                    BalanceChanged(amount);
+                    SendSMSUpdateBallance(amount);
+
+
+                return true;
+                }
+            return false;
+        }
+
+        public void SendSMSUpdateBallance(decimal amount)
+        {
+            // send SMS
+        }
     }
 }
